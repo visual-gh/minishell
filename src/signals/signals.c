1211a99e5221b3d11da1b27413eb851f4368c63b
visual-gh <github.molecular850@passmail.net>
2026-05-08 19:35:55 +0200

    sigaction->signal for simplicity... My head hurts

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -6,7 +6,7 @@
 /*   By: Visual <github.com/visual-gh>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/06 19:45:38 by Visual            #+#    #+#             */
-/*   Updated: 2026/05/07 00:25:28 by Visual           ###   ########.fr       */
+/*   Updated: 2026/05/08 18:28:29 by Visual           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,8 @@ static void	sigint_prompt(int sig)
 
 void	signals_prompt(void)
 {
-	struct sigaction	sa_int;
-	struct sigaction	sa_quit;
-
-	sa_int.sa_handler = sigint_prompt;
-	sigemptyset(&sa_int.sa_mask);
-	sa_int.sa_flags = 0;
-	sigaction(SIGINT, &sa_int, NULL);
-	sa_quit.sa_handler = SIG_IGN;
-	sigemptyset(&sa_quit.sa_mask);
-	sa_quit.sa_flags = 0;
-	sigaction(SIGQUIT, &sa_quit, NULL);
+	signal(SIGINT, sigint_prompt);
+	signal(SIGQUIT, SIG_IGN);
 }
 
 void	signals_child(void)
