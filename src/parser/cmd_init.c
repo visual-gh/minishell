4dec4dab8f7bd68097ac7d464e9a4469e527a1bb
Daniela Maria Da Silva Ornelas <danielad@c1r1s2.42lausanne.ch>
2026-06-08 16:24:39 +0200

    issue with saving code hasnt changed

--- src/parser/cmd_init.c
+++ src/parser/cmd_init.c
@@ -29,3 +29,43 @@ void    add_cmd(t_cmd **head, t_cmd *new_cmd)  //same as add token
         current = current->next;
     current->next = new_cmd;
 }
+
+
+static int  count_args(t_token *tok)
+{
+    int count;
+
+    count = 0;
+    while (tok && tok->type == TOK_WORD) // walk for as long as there is word type tokens
+    {
+        count++;
+        if (!tok->join)
+            break;
+        tok = tok->next;
+    }
+    return (count);
+}
+
+char    **make_argv(t_token *tok, int count)
+{
+    char    **argv;
+    int     i;
+
+    argv = malloc(sizeof(char *) * (count + 1));
+    if (argv == NULL)
+        return (NULL);
+    i = 0;
+    while (i < count)
+    {
+        argv[i] = ft_strdup(tok->value);
+        if (argv[i] == NULL)
+        {
+            //free(); //need free str array
+            return (NULL);
+        }
+        tok = tok->next;
+        i++;
+    } 
+    argv[i] = NULL;
+    return (argv);
+}
