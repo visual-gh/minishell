c9a5a162013f0a230b2f711702c3dda03f0ee03d
visual-gh <github.molecular850@passmail.net>
2026-08-04 21:50:53 +0200

    cd "" fix. Before: no such file in directory After: nothing shown, acts like cd .

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: Visual <github.com/visual-gh>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/17 18:51:20 by Visual            #+#    #+#             */
-/*   Updated: 2026/07/25 03:09:59 by Visual           ###   ########.fr       */
+/*   Updated: 2026/08/04 21:48:32 by Visual           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,8 @@ int	ft_cd(t_cmd *cmd, t_shell *shell)
 	target = get_target(cmd, shell);
 	if (target == NULL)
 		return (1);
+	if (*target == '\0')
+		target = ".";
 	if (getcwd(old_cwd, sizeof(old_cwd)) == NULL || chdir(target) == -1)
 		return (print_error("cd", target, strerror(errno)), 1);
 	env_set(&shell->envp, "OLDPWD", old_cwd);
